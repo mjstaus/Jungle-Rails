d29eb01f56e6b8224b8c37c7879b861caf2a30b8
Martha Staus <mjstaus@gmail.com>
2022-04-12 13:24:20 -0700

    FIX: Move authentication information to .env file

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
-  
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with  name: ENV["ADMIN_NAME"], 
+                                password: ENV["ADMIN_PASS"]
 
   def index
     @products = Product.order(id: :desc).all
