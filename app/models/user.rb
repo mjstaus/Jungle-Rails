00690aecbacb4de10c55f582b9c7d3b6be46d4bc
Martha Staus <mjstaus@gmail.com>
2022-04-26 13:29:19 -0700

    FEATURE: Add authenticate_with_credentials method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :password, length: { minimum: 4 }
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else 
+      nil
+    end
+  end
 end
+
